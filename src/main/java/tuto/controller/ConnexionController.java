bfb21e04503bbf979ce343009d543997a8986e4f
angeMartialTaboulaNoumo <martialtaboula@gmail.com>
2026-03-23 10:47:49 +0100

    Last

--- src/main/java/tuto/controller/ConnexionController.java
+++ src/main/java/tuto/controller/ConnexionController.java
@@ -3,8 +3,10 @@ package tuto.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -12,26 +14,34 @@ import org.springframework.web.bind.support.SessionStatus;
 public class ConnexionController {
 
 	@GetMapping( "/" )
-	public String page1() {
+	public String home() {
 		return "/connexion/home.html";
 	}
 
-	@GetMapping( "/connect" )
-	public String page2(String username, String password, Model model) {
+	@PostMapping( "/connect" )
+	public String connect(String username, String password, Model model) {
 		var sb = new StringBuilder( username );
 		if ( sb.reverse().toString().equals( password ) ) {
 			model.addAttribute( "connected", "Bonjour\t"+username );
-			return "connexion/bonjour.html";
+			return "redirect:/bonjour";
 		} else {
 			model.addAttribute( "alert", "mot de passe incorrect" );
+			model.addAttribute( "password", password );
+			model.addAttribute( "username", username );
 			return "connexion/home.html";
 		}
 	}
 	
-	@GetMapping( "/disconnect" )
-	public String disconnected(SessionStatus status) {
+	@PostMapping( "/disconnect" )
+	public String disconnected(SessionStatus status, RedirectAttributes ra) {
+		ra.addFlashAttribute( "alert", "Déconnexion effectuée avec succès" );
 		status.setComplete();
 		return "redirect:/";
+	}
+	
+	@GetMapping( "/bonjour" )
+	public String bonjour() {
+		return "/connexion/bonjour.html";
 	}
 
 }
